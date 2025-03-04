6329c5434f5f3bb6d165ce075a65641400de2732
orahbaloyi1 <orahbaloyi1@gmail.com>
2025-03-04 18:16:10 +0200

    Added bill per person variable

--- Day02/tip_calculator.py
+++ Day02/tip_calculator.py
@@ -6,6 +6,9 @@ bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 
-# Calculate the total amount
+# Calculate the total amount and per person
 total_amount = tip / 100 * bill + bill
+bill_per_person =  total_amount / people
+
 print(f"This is the total bill ${round(total_amount, 2)}")
+
