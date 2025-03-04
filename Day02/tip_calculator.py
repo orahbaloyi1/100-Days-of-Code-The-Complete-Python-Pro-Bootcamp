a7d6e4fcf638d925f02dae3bd67197e02a6397f0
orahbaloyi1 <orahbaloyi1@gmail.com>
2025-03-04 18:12:25 +0200

    Edited the variables

--- Day02/tip_calculator.py
+++ Day02/tip_calculator.py
@@ -1,6 +1,7 @@
 #Pprint function to print welcome message
 print("Welcome to the tip calculator!")
 
+# Get the bill amount, tip percentage, and number of people
 bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
